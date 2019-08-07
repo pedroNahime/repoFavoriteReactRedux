659de3f6ae66c680c501fcf6fc5ef232081869ad
pedronahime <J4r12t26@>
2019-08-07 00:04:58 -0300

    make it better

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+const Footer = ({count}) => <p>Voce tem {count} favoritos</p>
+
+Footer.propTypes ={
+    count: PropTypes.number.isRequired
+}
+
+const mapStateToProps = state =>({
+    count: state.favorites.data.length
+});
+
+export default connect(mapStateToProps)(Footer)
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react'
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Footer from "../components/Footer";
+
+import Main from '../pages/Main'
+
+const Routes = () => (
+    <BrowserRouter>
+        <Fragment>
+            <Switch>
+                <Route exact path="/" component={Main}/>
+            </Switch>
+            <Footer/>
+        </Fragment>
+    </BrowserRouter>
+);
+
+export default Routes
